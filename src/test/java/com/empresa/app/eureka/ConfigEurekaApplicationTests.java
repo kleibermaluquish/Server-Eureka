2d0c74f9ab759d9488bac6fc3fa0f5682cf11f53
kleibermaluquish <maluquish.19@gmail.com>
2019-11-26 18:39:01 -0500

    Se configuró el servidor Eureka

---
+++ src/test/java/com/empresa/app/eureka/ConfigEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
